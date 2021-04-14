38151683d5b420f987dd97e64fd59109e85468a8
paulobeckman <beckmam.paulo@hotmail.com>
2021-04-14 14:32:08 -0300

    first commit

---
+++ src/app/models/Product.js
@@ -0,0 +1,44 @@
+const db = require('../../config/db')
+const Base = require('./Base')
+
+Base.init({ table: 'products' })
+
+module.exports = {
+    ...Base,
+    async files(id){
+        const results = await db.query(`
+            SELECT * 
+            FROM files 
+            WHERE product_id = $1`, [id]) 
+        return results.rows
+    },
+    async search(params) {
+        const { filter, category } = params
+
+        let query = "",
+            filterQuery = `WHERE`
+
+        if (category){
+            filterQuery = `${filterQuery}
+            products.category_id = ${category}
+            AND`
+        }
+
+        filterQuery = `
+            ${filterQuery}
+            products.name ilike '%${filter}%'
+            OR products.description ilike '%${filter}%'
+        `
+
+        query = `
+            SELECT products.*, 
+                categories.name AS category_name
+            FROM products
+            LEFT JOIN categories ON (categories.id = products.category_id)
+            ${filterQuery} 
+        `
+
+        const results = await db.query(query)
+        return results.rows
+    }
+}
