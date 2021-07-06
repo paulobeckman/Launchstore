8e90df546516a52aede3297dbab9f79d5401bd2a
paulobeckman <beckmam.paulo@hotmail.com>
2021-07-06 11:32:13 -0300

    Change Order Status

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -10,6 +10,7 @@ routes.post('/', onlyUsers, OrderController.post)
     .get('/', onlyUsers, OrderController.index)
     .get('/sales', onlyUsers, OrderController.sales)
     .get('/:id', onlyUsers, OrderController.show)
+    .post('/:id/:action', onlyUsers, OrderController.update)
 
 
 module.exports = routes
